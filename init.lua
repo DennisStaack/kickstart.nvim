bc0d59c98727c8cb79858f8b628c7c1678afa714
DennisStaack <dennis@familie-staack.de>
2025-06-21 17:34:34 +0200

    merge remnant

--- init.lua
+++ init.lua
@@ -159,20 +159,12 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-<<<<<<< HEAD
 vim.opt.scrolloff = 7
 
 -- Set Tabstop
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-=======
-vim.o.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
 vim.o.confirm = true
->>>>>>> upstram/master
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
